e4919c281b0769c9c35303830501456527f8aa4f
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-15 21:30:36 +0300

    start2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGallaryMarkup(galleryItems);
+let instance;
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// const lightbox = new SimpleLightbox(".gallery__link", {
+//  captionsData: "alt",
+//  captionDelay: 250,
+// });
+
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
+function openGallery(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
+galleryList.addEventListener("click", openGallery);
+
+//galleryList.addEventListener("keyup", closeGallery);
+
+function closeGallery(event) {
+  if(event.code === 'Escape') {
+    document.removeEventListener('keyup', closeGallery);
+    instance.close();
+}
+}
+
